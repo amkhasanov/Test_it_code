11bfabafa2d6855d7eb3be5bdd3451880eea64ff
amkhasanov <ainur0225@gmail.com>
2022-03-05 18:26:50 +0500

    Поправили согласно pep-8

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
     welcome_sti = open('Sticker/AnimatedSticker.tgs', 'rb')
     bot.send_sticker(message.chat.id, welcome_sti)
 
-    bot.send_message(message.chat.id, f"Приветствую тебя ученик Python-а! {format(message.from_user.username, '')}",
-    parse_mode='html')
+    bot.send_message(message.chat.id, f"Приветствую тебя ученик Python-а! {message.from_user.username}",
+                     parse_mode='html')
+
 
 @bot.message_handler(content_types=['text'])
-
 def get_text_message(message):
     username = message.from_user.username
     msg = f"Здравствуй, {username}"
